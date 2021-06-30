e80fa2da54563e853dcbf441f025ce420a58cf43
spencer fox <spencerfox@outlook.com>
2021-06-30 18:16:08 -0400

    URI

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from os import environ
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
-    'DATABASE_URL', 'sqlite:///notepad.sqlite')
+    'DATABASE_URI', 'sqlite:///notepad.sqlite')
 
 db = SQLAlchemy(app)
 class Task(db.Model):
