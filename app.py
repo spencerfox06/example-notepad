081bbd732752ef16363b7ca4b255509a8cb53cc5
spencer fox <spencerfox@outlook.com>
2021-06-30 16:30:37 -0400

    postgres

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
+    'DATABASE_URL', 'sqlite:///notepad.sqlite')
+
+db = SQLAlchemy(app)
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String)
+
 
 @app.route('/')
 def index():
     return 'Spencer says Hello World'
+
+@app.route('/api/tasks-postgres')
+def tasksPostGres():
+    tasks = db.session.query(Task)
+    data = []
+
+    for task in tasks:
+        item = {
+            'id': task.id,
+            'description': task.description
+         }
+        data.append(item)
+
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
